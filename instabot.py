bf5d46a4ccbc9389ba57adcee298ed1cf99f4a31
ind0ver <53695788+ind0ver@users.noreply.github.com>
2019-11-15 00:19:19 +0300

    Add files via upload

---
+++ instabot.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class InstaBot:
+    def __init__(self, username, password):  
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Chrome()
+
+    def login(self):
+        bot = self.bot
+        bot.get('https://www.instagram.com/accounts/login/')
+
+        time.sleep(5)
+        email = bot.find_element_by_name('username')
+        password = bot.find_element_by_name('password')
+
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+    def like_posts(self, hashtag):
+        count = 0
+        bot = self.bot
+        bot.get(f'https://www.instagram.com/explore/tags/{hashtag}/')
+        time.sleep(3)
+
+        for i in range(1, 3):  # scrolls down twice
+            bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            time.sleep(2)
+            posts = bot.find_elements_by_class_name('v1Nh3')
+            links = [elem.find_element_by_css_selector(
+                'a').get_attribute('href') for elem in posts]
+
+            for link in links:  
+                count += 1
+                if count < 45:  # number of top posts to leave a like on
+                    bot.get(link)
+                    try:
+                        div = bot.find_elements_by_class_name('dCJp8')
+                        lov = [el.find_element_by_class_name('glyphsSpriteHeart__outline__24__grey_9').click() for el in div]
+                        print('liked photo')
+                        time.sleep(5)
+                    except Exception as ex:
+                        time.sleep(10)
+                else:  # after 45 times the bot exits
+                    bot.close()
+
+
+obj = InstaBot('', '')  # type in your name and password here
+obj.login()
+obj.like_posts('')  # type the hashtag name (without #)
